01e17cde8f3fe7868a2eb151b766e2287e1dfcfd
unknown <peppy1698@gmail.com>
2021-11-28 20:45:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+
+const getDadJoke = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "Sorry, no jokes available today!";
+    }
+}
+
+button.addEventListener('click', addNewJoke)
